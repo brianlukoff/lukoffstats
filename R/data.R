90171f2580b48631d49c8707a1681e0ffe4aaef6
Brian Lukoff <brian.lukoff@gmail.com>
2022-09-10 16:29:47 -0500

    Change name of Starbucks and McDonalds data sets.

--- R/data.R
+++ R/data.R
@@ -9,11 +9,11 @@ inspections.routine <- read.csv("http://welkin.brianlukoff.com/data/inspections.
 inspections.followup <- read.csv("http://welkin.brianlukoff.com/data/inspections.followup.csv")
 inspections.2020 <- read.csv("http://welkin.brianlukoff.com/data/inspections.2020.csv")
 inspections.2021 <- read.csv("http://welkin.brianlukoff.com/data/inspections.2021.csv")
-inspections.starbucks <- inspections.routine[grep("Starbucks", inspections.routine$name, ignore.case=TRUE),]
-inspections.mcdonalds <- inspections.routine[grep("McDonald", inspections.routine$name, ignore.case=TRUE),]
+starbucks <- inspections.routine[grep("Starbucks", inspections.routine$name, ignore.case=TRUE),]
+mcdonalds <- inspections.routine[grep("McDonald", inspections.routine$name, ignore.case=TRUE),]
 colleges <- read.csv("http://welkin.brianlukoff.com/data/colleges.csv")
 
 usethis::use_data(retail, phone.cases, network, mortgages, health, civil.rights, airbnb,
-                  inspections.routine, inspections.followup, inspections.starbucks, inspections.mcdonalds,
+                  inspections.routine, inspections.followup, starbucks, mcdonalds,
                   inspections.2020, inspections.2021, colleges,
                   overwrite=T)
